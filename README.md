d7087a31fe98542871df0378186dd18179bd9afa
Johan Witters <johan@witters.be>
2018-01-02 19:12:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# How to call different setter methods dynamically based on the input data order/name in java?
+
+https://stackoverflow.com/questions/48064761/how-to-call-different-setter-methods-dynamically-based-on-the-input-data-order-n/48065497#48065497
---
+++ init/src/main/java/com/johanw/stackoverflow/dynamicinit/init/PopulateObjects2.java
@@ -0,0 +1,30 @@
+    package com.johanw.stackoverflow.dynamicinit.init;
+
+    import java.lang.reflect.InvocationTargetException;
+    import java.lang.reflect.Method;
+
+    public class PopulateObjects2 {
+        interface InitialiseFieldAction {
+            void initialise(MyObjectImpl object, String value);
+        }
+
+        private static String[] initMethods = {
+                "setName", "setStreet", "setNumberAsString"
+        };
+
+        private static Method getMethod(int index) throws NoSuchMethodException {
+            if ((index < 0) || (index > initMethods.length - 1)) return null;
+            Class clazz = MyObjectImpl.class;
+            return clazz.getMethod(initMethods[index], String.class);
+        }
+
+        public static MyObjectImpl retrieveWithValues(String[] values) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+            if (values.length != initMethods.length) return null;   // Handle the fact you don't have the same amount of initialisations as the amount of fields
+            MyObjectImpl o = new MyObjectImpl();
+            for (int i = 0; i < initMethods.length; i++) {
+                Method m = getMethod(i);
+                m.invoke(o, values[i]);
+            }
+            return o;
+        }
+    }
